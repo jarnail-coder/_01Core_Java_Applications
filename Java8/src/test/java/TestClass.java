d5551eac0c690df845c9c2475e298c9c46068c20
jarnail-coder <jarnailsinghcoder@gmail.com>
2017-05-27 14:36:00 +0530

    Added Amana for Testing

--- Java8/src/test/java/TestClass.java
+++ Java8/src/test/java/TestClass.java
@@ -1,7 +1,7 @@
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Just For Testing");
+		System.out.println("JustMy Name is Aman For  You Know Testing");
 		System.out.println("++++++++++++Table Of Four+++++++");
 		for (int i = 1; i < 20; i++) {
 			System.out.println("5 * " + i + "=" + (i * 5));
