8071deb6043bd1838d3471039bcb08fa618f33e3
jarnail-coder <jarnailsinghcoder@gmail.com>
2017-05-27 14:33:20 +0530

    Jarnail Singh Added for Testing

--- Java8/src/test/java/TestClass.java
+++ Java8/src/test/java/TestClass.java
@@ -1,7 +1,7 @@
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Just For Testing");
+		System.out.println("You know This is Jarnail Singh For Testing");
 		System.out.println("++++++++++++Table Of Four+++++++");
 		for (int i = 1; i < 20; i++) {
 			System.out.println("5 * " + i + "=" + (i * 5));
