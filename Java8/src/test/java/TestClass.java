ad907c5f4b82d481ca8b3ed24bcbb67e42a3586d
jarnail-coder <jarnailsinghcoder@gmail.com>
2017-05-27 14:17:38 +0530

    Commiting table tof 2 locally

--- Java8/src/test/java/TestClass.java
+++ Java8/src/test/java/TestClass.java
@@ -3,5 +3,8 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Just For Testing");
 		System.out.println("Another Test");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("2 * " + i + "=" + (i * 2));
+		}
 	}
 }
